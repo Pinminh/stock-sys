63809ef721024f9badf56be2af0b3d253dabd030
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-03-29 20:53:56 +0700

    Add tests for StockFeeder observer notification

--- src/test/java/StockFeederTest.java
+++ src/test/java/StockFeederTest.java
@@ -8,13 +8,19 @@ import org.junit.Rule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
+import java.util.Map;
+
 import com.myproject.StockFeeder;
 import com.myproject.StockTickerView;
 import com.myproject.Stock;
+import com.myproject.StockAlertView;
+import com.myproject.StockPrice;
+import com.myproject.StockRealtimePriceView;
 
 
 public class StockFeederTest {
     private static StockFeeder feeder;
+    private double doubleError = 0.00001;
 
     @BeforeClass
     public static void setupBeforeAll() {
@@ -140,6 +146,59 @@ public class StockFeederTest {
         assertTrue(feeder.hasViewerWithStockCode(viewer, "codeE"));
         feeder.unregisterViewer("codeE", viewer);
         assertFalse(feeder.hasViewerWithStockCode(viewer, "codeE"));
+    }
+
+    @Test
+    public void notifyAlertViewWithMultipleStocks() {
+        feeder.addStock(new Stock("codeA", "nameA"));
+        feeder.addStock(new Stock("codeB", "nameB"));
+        StockAlertView alertView = new StockAlertView(60, 20);
+        feeder.registerViewer("codeA", alertView);
+        feeder.registerViewer("codeB", alertView);
+        StockPrice[] prices = {
+            new StockPrice("codeA", 30.0, 25, 1743250124),
+            new StockPrice("codeA", 40.0, 25, 1743250136),
+            new StockPrice("codeB", 10.0, 25, 1743250181),
+            new StockPrice("codeA", 62.0, 25, 1743250124),
+            new StockPrice("codeB", 10.0, 25, 1743250181),
+            new StockPrice("codeA", 52.0, 25, 1743250124),
+        };
+        for (StockPrice price : prices) feeder.notify(price);
+        Map<String, Double> priceMap = alertView.get();
+        assertEquals(62.0, priceMap.get("codeA"), doubleError);
+        assertEquals(10.0, priceMap.get("codeB"), doubleError);
+    }
+
+    @Test
+    public void notifyMultipleViewsWithMultipleStocks() {
+        feeder.addStock(new Stock("codeA", "nameA"));
+        feeder.addStock(new Stock("codeB", "nameB"));
+        StockAlertView alertView = new StockAlertView(60, 20);
+        feeder.registerViewer("codeA", alertView);
+        feeder.registerViewer("codeB", alertView);
+        StockTickerView tickerView = new StockTickerView();
+        feeder.registerViewer("codeA", tickerView);
+        feeder.registerViewer("codeB", tickerView);
+        StockRealtimePriceView realtimeView = new StockRealtimePriceView();
+        feeder.registerViewer("codeA", realtimeView);
+        feeder.registerViewer("codeB", realtimeView);
+        StockPrice[] prices = {
+            new StockPrice("codeA", 70.0, 25, 1743250124),
+            new StockPrice("codeA", 40.0, 25, 1743250136),
+            new StockPrice("codeA", 40.0, 25, 1743250136),
+            new StockPrice("codeB", 10.0, 25, 1743250181),
+            new StockPrice("codeA", 62.0, 25, 1743250194),
+            new StockPrice("codeB", 30.0, 25, 1743250201),
+            new StockPrice("codeA", 52.0, 25, 1743250224),
+            new StockPrice("codeA", 52.0, 25, 1743250224),
+        };
+        for (StockPrice price : prices) feeder.notify(price);
+        Map<String, Double> alertMap = alertView.get();
+        Map<String, Double> realtimeMap = realtimeView.get();
+        assertEquals(62.0, alertMap.get("codeA"), doubleError);
+        assertEquals(10.0, alertMap.get("codeB"), doubleError);
+        assertEquals(52.0, realtimeMap.get("codeA"), doubleError);
+        assertEquals(30.0, realtimeMap.get("codeB"), doubleError);
     }
 
     @Rule
