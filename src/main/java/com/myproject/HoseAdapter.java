e86bd171f1c372ec18147daada29d5903a28f755
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2025-03-29 21:22:52 +0700

    Add compatibility for HoseAdapter

--- src/main/java/com/myproject/HoseAdapter.java
+++ src/main/java/com/myproject/HoseAdapter.java
@@ -1,23 +1,29 @@
 package com.myproject;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class HoseAdapter implements PriceFetcher {
     private HosePriceFetchLib hoseLib;
     private List<String> stockCodes;
  
     public HoseAdapter(HosePriceFetchLib hoseLib, List<String> stockCodes) {
-        // TODO: Implement constructor
+        this.hoseLib = hoseLib;
+        this.stockCodes = stockCodes;
     }
 
     @Override
     public List<StockPrice> fetch() {
-        // TODO: Fetch stock data and convert it to StockPrice list
-        return null;
+        List<StockPrice> stockPrices = new ArrayList<>();
+        List<HoseData> hoseList = hoseLib.getPrices(stockCodes);
+        for (HoseData hoseData : hoseList) {
+            stockPrices.add(convertToStockPrice(hoseData));
+        }
+        return stockPrices;
     }
 
+
     private StockPrice convertToStockPrice(HoseData hoseData) {
-        // TODO: Convert HoseData to StockPrice
-        return null;
+        return new StockPrice(hoseData.getStockCode(), hoseData.getPrice(), hoseData.getVolume(), hoseData.getTimestamp());
     }
 }
